d0afc4d2ba1f70188beafe81fb0a377ecc96cfe4
sapireddyrahul@gmail.com <ssdx5@umsystem.edu>
2023-05-28 01:33:42 -0500

    Section_3

--- 2.SV datatypes/25.A35.sv
+++ 2.SV datatypes/25.A35.sv
@@ -3,21 +3,25 @@ Now add all the elements of the fixed-size array to the Queue in such a way that
 the last element of the Queue. Print all the elements of both fixed-size array and Queue on Console.*/
 
 module tb;
-  reg arr1[0:19];
+  bit arr1[0:19];
   int arr2[$];
+  
   initial begin
     for(int i=0;i<20;i++) begin
       arr1[i] = $urandom();
     end
-    $display("values of arr1: %0p",1arr);
-    
+    $display("values of arr1: %0p",arr1);
+
     for(int i=0;i<20;i++) begin
-      
+      arr2[i] = i*5;
     end
-    
-    for(int i=20,i>=19;i--) begin
-      arr2 = arr2.push_back(arr1[i]);
+    $display("values of arr2: %0p",arr2);
+
+    for(int i=0;i<20;i++) begin
+      arr2.push_back(arr1[i]);
     end
-    $display("values of the arr2: %0p",arr2);
+    $display("values of the arr2 after adding arr1: %0p",arr2);
+
   end
+
 endmodule
