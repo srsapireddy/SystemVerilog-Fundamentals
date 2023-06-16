6962e470a8dbdcee89f55cc45cd30857c47befe8
sapireddyrahul@gmail.com <ssdx5@umsystem.edu>
2023-06-16 01:12:37 -0500

    SystemVerilog Files

--- 5.Randomization/15.A63.sv
+++ 5.Randomization/15.A63.sv
@@ -15,5 +15,30 @@ Add Constraint Code and Testbench top code to  the Design.*/
     //////////////////Add constraint and TB top code
 */
 
+class generator;
+  rand bit [4:0] a;
+  rand bit [5:0] b;
+  
+  constraint data {
+    a inside {[0:8]};
+    b inside {[0:5]};
+  }
+endclass
 
+module tb;
+  generator g;
+  int i = 0;
+  int status = 0;
+  int err = 0;
+  initial begin
+    for(i=0;i<20;i++) begin
+      g = new();
+      status = g.randomize(); 
+        $display("Values for a and b: %0d and %0d", g.a, g.b);
+        if(!status)
+         err = err + 1;
+      end
+    $display("Value of the status where randomization fails: %0d", err);
+  end
+endmodule
 
