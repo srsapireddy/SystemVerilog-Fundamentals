2289b2308c1853a1fb81474b6d455f6e9567389e
sapireddyrahul@gmail.com <ssdx5@umsystem.edu>
2023-06-22 14:12:24 -0500

    SystemVerilog Files

--- 7.Interface/4.Prefer_LOGIC_over_REG_and_WIRE_in_Inteface.sv
+++ 7.Interface/4.Prefer_LOGIC_over_REG_and_WIRE_in_Inteface.sv
@@ -1,9 +1,12 @@
 // testbench.sv
 interface add_if;
-  logic [3:0] a;
-  logic [3:0] b;
-  logic [4:0] sum;
+  wire [3:0] a;
+  wire [3:0] b;
+  wire [4:0] sum;
 endinterface
+// interface with all reg type. we are not allowed to connect variable in interface to the output port of DUT
+// interface with all wire type, we are not allowed to apply stimulus using initial or always 
+
 
 module tb;
   add_if aif();
@@ -11,17 +14,16 @@ module tb;
   add dut (.b(aif.b), .a(aif.a), .sum(aif.sum)); // mapping by name
   
   initial begin
-    aif.a = 4;
-    aif.b = 4;
-    #10;
-    aif.a = 3;
-    #10;
-    aif.b = 7;
+    aif.a = 1;
+    aif.b = 3;
+   
   end
   
   initial begin
     $dumpfile("dump.vcd");
     $dumpvars;
+    #100;
+    $finish();
   end
 endmodule
 
@@ -31,6 +33,6 @@ module add (
   output [4:0] sum,
 );
 
-assign sum = a + b;
+  assign sum = a + b;
 
 endmodule
